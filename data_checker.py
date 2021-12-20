77edf5294aeae43e14e1fb323732d397c2841940
GlistenSTAR <glistenstar00@gmail.com>
2021-12-19 18:14:36 -0600

    add the event on ticket list for two type

--- data_checker.py
+++ data_checker.py
@@ -1,5 +1,3 @@
-import re
-
 import requests
 from bs4 import BeautifulSoup
 from dateutil import parser
@@ -13,106 +11,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-
 def get_event_name_and_date(url):
     if 'axs.' in url:
         return check_axs(url)
-
-
-def open_driver(self, 
-                    use_proxy=False,
-                    user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36.',
-                    headless=False):
-        random_proxy = random.choice(self.proxies)
-        auth, ip_port = random_proxy.split('@')
-        user, pwd = auth.split(':')
-        ip, port = ip_port.split(':')
-        manifest_json = """
-        {
-            "version": "1.0.0",
-            "manifest_version": 2,
-            "name": "Chrome Proxy",
-            "permissions": [
-                "proxy",
-                "tabs",
-                "unlimitedStorage",
-                "storage",
-                "<all_urls>",
-                "webRequest",
-                "webRequestBlocking"
-            ],
-            "background": {
-                "scripts": ["background.js"]
-            },
-            "minimum_chrome_version":"22.0.0"
-        }
-        """
-
-        background_js = """
-        var config = {
-                mode: "fixed_servers",
-                rules: {
-                  singleProxy: {
-                    scheme: "http",
-                    host: "%s",
-                    port: parseInt(%s)
-                  },
-                  bypassList: ["localhost"]
-                }
-              };
-
-        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
-
-        function callbackFn(details) {
-            return {
-                authCredentials: {
-                    username: "%s",
-                    password: "%s"
-                }
-            };
-        }
-
-        chrome.webRequest.onAuthRequired.addListener(
-                    callbackFn,
-                    {urls: ["<all_urls>"]},
-                    ['blocking']
-        );
-        """ % (ip, port, user, pwd)
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        if headless:
-            pass
-            # chrome_options.add_argument('--headless')
-
-        if use_proxy:
-            pluginfile = 'proxy_auth_plugin.zip'
-            with zipfile.ZipFile(pluginfile, 'w') as zp:
-                zp.writestr("manifest.json", manifest_json)
-                zp.writestr("background.js", background_js)
-            chrome_options.add_extension(pluginfile)
-            time.sleep(1)
-        if user_agent:
-            chrome_options.add_argument('--user-agent=%s' % user_agent)
-        try:
-            driver = webdriver.Chrome(
-                'chromedriver', chrome_options=chrome_options)
-        except Exception as E:
-            return self.open_driver()
-
-        # Check if it's working
-        driver.get('https://www.google.com/')
-        try:
-            driver.find_element_by_id("L2AGLb").click()
-            time.sleep(0.5)
-        except:
-            pass
-
-        try:
-            driver.find_element_by_css_selector('input[name="q"]')
-        except:
-            driver.quit()
-            return self.open_driver()
-        return driver
 
 def make_request(url):
     headers = {
@@ -124,14 +25,73 @@ def make_request(url):
         return BeautifulSoup(r.content, 'html.parser')
     print(r.status_code, url)
 
-
 def check_axs(url):
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36.'
+    chrome_options.add_argument('--user-agent=%s' % user_agent)
+    # chrome_options.add_argument('--headless')
+    while True:
+        try:
+            driver = webdriver.Chrome(
+                'chromedriver', chrome_options=chrome_options)
+            break
+        except Exception as E:
+            pass
+
+    # Check if it's working
+    driver.get('https://www.google.com/')
+    try:
+        driver.find_element_by_id("L2AGLb").click()
+        time.sleep(0.5)
+    except:
+        pass
+
+    driver.find_element_by_css_selector('input[name="q"]')
+    time.sleep(1)
+
+
     driver.get(url)
+    time.sleep(3)
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@class="btn--single btn btn-default"]'))
+        )
+    except:
+        try:
+            element = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="ackBtn"]'))
+            )
+        except:
+            flag = False
+    finally:
+        flag = True
     time.sleep(2)
-    driver.find_element_by_class_name('btn--single btn btn-default').click()
-    time.sleep(3000)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    if flag:
+        try:
+            driver.find_element_by_xpath('//*[@class="btn--single btn btn-default"]').click()
+        except:
+            driver.find_element_by_xpath('//button[@id="ackBtn"]').click()
+        time.sleep(3)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        try:
+            name = soup.find('div', {'class':'event-summary-headline__event'}).text.strip()
+        except:
+            name = soup.find('h1', {'class':'c-marquee__headliner h-ellipsis'}).find_next('a').text.strip()
+        try:
+            venue = soup.find('span', {'class': 'venue'}).text.strip()
+        except:
+            venue = soup.find('div', {'class': 'c-event-info__venue c-event-info__venue--sticky-nav'}).text.strip()
+        name = name + "-/-" + venue
+        try: 
+            date = soup.find('span', {'class':'event-header-date'}).text.strip()
+            date = parser.parse(date.split('–')[0]).strftime('%Y-%m-%d')
+        except:
+            date = soup.find('span', {'class':'c-event-info__table-date'}).text.strip()
+            date = parser.parse(date.split('–')[0]).strftime('%Y-%m-%d')
+    # print(f'date>>>>>>>{date}')
+    # time.sleep(3000)
+    print("Add the Event on Google sheet.....")
     return name, date
 
 # for url in ['https://basscanyonfestival.frontgatetickets.com/event/i8co6kruzz405we8',
